3940ab141afdc43be22ca24e813a54f5162c064f
Elliot Nunn <elliotnunn@me.com>
2021-10-09 21:39:41 +0800

    Correct quoting behaviour

--- quote.go
+++ quote.go
@@ -1,6 +1,54 @@
 package main
 
-// TODO: make this much better
-func quote(s macstring) macstring {
-	return macstring("'") + s + macstring("'")
+func quote(in macstring) macstring {
+	out := make([]byte, 0, len(in)+2)
+
+	needQuote := false
+
+	// Need to quote empty string
+	if len(in) == 0 {
+		needQuote = true
+	}
+
+	// Need to quote if any of these characters are present
+	for _, c := range []byte(in) {
+		switch c {
+		case '#', ' ', '\t', '\r', ';', '&', '|', '(', ')', '\'', '"',
+			'/', '\\', '{', '}', '`', '?', '[', ']', '+', '*', '<', '>',
+			0xb6, // ∂
+			0xc5, // ≈
+			0xc7, // «
+			0xc8, // »
+			0xa8, // ®
+			0xb3, // ≥
+			0xb7, // ∑
+			0xc9: // …
+			needQuote = true
+			break
+		}
+	}
+
+	if needQuote {
+		out = append(out, '\'')
+	}
+
+	// The escape character is ∂
+	for _, c := range []byte(in) {
+		switch c {
+		case 0xb6:
+			out = append(out, '\'', 0xb6, 0xb6, '\'')
+		case '\r':
+			out = append(out, '\'', 0xb6, 'n', '\'')
+		case '\'':
+			out = append(out, '\'', 0xb6, '\'', '\'')
+		default:
+			out = append(out, c)
+		}
+	}
+
+	if needQuote {
+		out = append(out, '\'')
+	}
+
+	return macstring(out)
 }
---
+++ quote_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+var quoteTestPairs = []string{
+	"", "''",
+	" ", "' '",
+	"x∂", "'x'∂∂''",
+	"x\n", "'x'∂n''",
+	"abc", "abc",
+	"abc def", "'abc def'",
+}
+
+func TestQuote(t *testing.T) {
+	for i := 0; i*2 < len(quoteTestPairs); i += 2 {
+		in := unicodeToMacOrPanic(quoteTestPairs[i])
+		out := unicodeToMacOrPanic(quoteTestPairs[i+1])
+
+		if quote(in) != out {
+			t.Fatalf("expected %q, got %q", out, quote(in))
+		}
+	}
+}
