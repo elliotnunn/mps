35bbd7c5bd60936684fe1e2e9fd8e07b240860ed
Elliot Nunn <elliotnunn@me.com>
2022-05-16 10:33:30 +0800

    Handle the occasional Mac Roman file with LFs

--- roman.go
+++ roman.go
@@ -12,7 +12,7 @@ type macstring string
 
 var macToUnicodeTable = [...]rune{
 	0x0000, 0x0001, 0x0002, 0x0003, 0x0004, 0x0005, 0x0006, 0x0007,
-	0x0008, 0x0009, 0xfffd, 0x000b, 0x000c, 0x000a, 0x000e, 0x000f,
+	0x0008, 0x0009, 0x000a, 0x000b, 0x000c, 0x000d, 0x000e, 0x000f,
 	0x0010, 0x0011, 0x0012, 0x0013, 0x0014, 0x0015, 0x0016, 0x0017,
 	0x0018, 0x0019, 0x001a, 0x001b, 0x001c, 0x001d, 0x001e, 0x001f,
 	0x0020, 0x0021, 0x0022, 0x0023, 0x0024, 0x0025, 0x0026, 0x0027,
@@ -152,11 +152,21 @@ var macStripTab = [...]byte{
 
 func macToUnicode(mac macstring) string {
 	var buf bytes.Buffer
-	for i := 0; i < len(mac); i++ {
-		r := macToUnicodeTable[mac[i]]
-		if r != 0xfffd { // essentially, skip LFs
-			buf.WriteRune(r)
+
+	// State machine converts CR/LF/CRLF all to LF
+	prevWasCR := false
+	for _, roman := range []byte(mac) {
+		if roman == '\r' {
+			buf.WriteByte('\n')
+		} else if roman == '\n' {
+			if !prevWasCR {
+				buf.WriteByte('\n')
+			}
+		} else {
+			buf.WriteRune(macToUnicodeTable[roman])
 		}
+
+		prevWasCR = roman == '\r'
 	}
 	return buf.String()
 }
